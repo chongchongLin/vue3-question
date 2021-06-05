784a9219c481ca1dbc189317a2be446ac97a196a
lin98 <1726928218@qq.com>
2021-06-05 20:20:15 +0800

    feat:工资计算器初步

--- vue3-question/src/components/Wage.vue
+++ vue3-question/src/components/Wage.vue
@@ -1,24 +1,67 @@
-
 <template>
-<el-time-select
-  v-model="value"
-  start='08:30'
-  step='00:15'
-  end='18:30'
-  placeholder="选择时间">
-</el-time-select>
+  <div class="container">
+    <el-input
+      v-model="wage"
+      placeholder="请输入你目前的工资"
+      class="wage-ipt"
+    ></el-input>
+    <el-radio-group v-model="time" class="wage-ipt">
+      <el-radio-button
+        :label="item.name"
+        v-for="item in timeList"
+        :key="item.id"
+      ></el-radio-button>
+    </el-radio-group>
+    <el-input
+      v-model="hourWage"
+      placeholder="当前的时薪"
+      class="wage-ipt"
+    ></el-input>
+  </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        value: ''
-      };
+export default {
+  data() {
+    return {
+      value: "",
+      wage: "",
+      hourWage: "",
+      time: "995",
+      timeList: [
+        { name: "996", id: 0 ,time:12},
+        { name: "955", id: 1 ,time:8},
+        { name: "大小周", id: 2},
+      ],
     }
-  }
+  },
+   watch: {
+      time:{
+        handler(newVal,oldVal){
+          const result = this.findTime(newVal);
+          let time = result>-1 ? this.timeList[result].time : '';
+          this.hourWage = Math.round(this.wage/(time*30));
+
+        }
+      }
+    },
+    methods:{
+      findTime(name){
+         return this.timeList.findIndex((item)=>item.name == name)
+      }
+    }
+};
 </script>
 
-<style>
+<style lang="scss">
 
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+.wage-ipt {
+  width: 300px;
+}
 </style>
