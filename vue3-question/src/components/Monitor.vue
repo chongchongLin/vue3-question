91b2ab64273765fa112e0b855726cacbca8cdb69
林远超 <1726928218@qq.com>
2021-06-15 10:25:29 +0800

    fix:bug

--- vue3-question/src/components/Monitor.vue
+++ vue3-question/src/components/Monitor.vue
@@ -4,12 +4,21 @@
       <div class="item" v-for="item in list" :key="item.id">
         <div class="close-btn" @click="close(item)"></div>
         <span v-if="item.show">{{ item.content }}</span>
-        <span v-if="!item.show && !item.isadd" @click="add(item)">添加</span>
+        <span class="add" v-if="!item.show && !item.isadd" @click="add(item)"
+          >添加</span
+        >
         <span v-if="item.isadd">添加中ing</span>
       </div>
     </div>
     <div class="list">
-        <div class="item" v-for="item in list1" :key="item.id" @click="select(item)">{{item.content}}</div>
+      <div
+        class="item"
+        v-for="item in list1"
+        :key="item.id"
+        @click="select(item)"
+      >
+        {{ item.content }}
+      </div>
     </div>
   </div>
 </template>
@@ -20,26 +29,26 @@ export default {
     return {
       show: true,
       list: [
-        { id: 1 ,content:1},
-        { id: 2,content:2 },
-        { id: 3,content:3 },
-        { id: 4 ,content:4},
-        { id: 5 ,content:5},
-        { id: 6 ,content:6},
-        { id: 7,content:7 },
-        { id: 8 ,content:8},
-        { id: 9 ,content:9},
+        { id: 1, content: 1 },
+        { id: 2, content: 2 },
+        { id: 3, content: 3 },
+        { id: 4, content: 4 },
+        { id: 5, content: 5 },
+        { id: 6, content: 6 },
+        { id: 7, content: 7 },
+        { id: 8, content: 8 },
+        { id: 9, content: 9 },
       ],
       list1: [
-        { id: 1 ,content:1},
-        { id: 2,content:2 },
-        { id: 3,content:3 },
-        { id: 4 ,content:4},
-        { id: 5 ,content:5},
-        { id: 6 ,content:6},
-        { id: 7,content:7 },
-        { id: 8 ,content:8},
-        { id: 9 ,content:9},
+        { id: 1, content: 1 },
+        { id: 2, content: 2 },
+        { id: 3, content: 3 },
+        { id: 4, content: 4 },
+        { id: 5, content: 5 },
+        { id: 6, content: 6 },
+        { id: 7, content: 7 },
+        { id: 8, content: 8 },
+        { id: 9, content: 9 },
       ],
     };
   },
@@ -55,7 +64,8 @@ export default {
       for (let item of this.list) {
         if (index.id == item.id) {
           item.show = false;
-        }else{
+        } else {
+          item.isadd = false;
           item.show = true;
         }
       }
@@ -67,23 +77,22 @@ export default {
         }
       }
     },
-    select(selectItem){
-       for(let item of this.list){
-           if(item.isadd){
-               item.isadd = false;
-               item.show = true;
-               item.content = selectItem.content
-           }
-       }
-
-    }
+    select(selectItem) {
+      for (let item of this.list) {
+        if (item.isadd) {
+          item.isadd = false;
+          item.show = true;
+          item.content = selectItem.content;
+        }
+      }
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.all{
-    display: flex;
+.all {
+  display: flex;
 }
 .content {
   width: 1000px;
@@ -97,6 +106,7 @@ export default {
     border: 1px solid red;
     line-height: 150px;
     position: relative;
+    text-align: center;
     .close-btn {
       width: 20px;
       height: 20px;
@@ -105,18 +115,23 @@ export default {
       position: absolute;
       right: 15px;
       top: 15px;
+      cursor: pointer;
+    }
+    .add {
+      cursor: pointer;
     }
   }
 }
-.list{
-    width: 300px;
-    flex-direction: column;
-    display: flex;
-    .item{
-        width: 100%;
-        border: 1px solid black;
-        height: 50px;
-        line-height: 50px;
-    }
+.list {
+  width: 300px;
+  flex-direction: column;
+  display: flex;
+  .item {
+    width: 100%;
+    border: 1px solid black;
+    height: 50px;
+    text-align: center;
+    line-height: 50px;
+  }
 }
 </style>
